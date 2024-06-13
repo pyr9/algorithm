494f5f789c7ee53ba445170be0e0791d3a7dc512
panyuro <panyuro@163.com>
2024-06-13 21:33:09 +0800

    【滑动窗口】不含有重复字符的 最长子串的长度

--- src/main/java/com/pyr/string/slidingWindow/MaxLengthOfSubString.java
+++ src/main/java/com/pyr/string/slidingWindow/MaxLengthOfSubString.java
@@ -47,3 +47,26 @@ public class MaxLengthOfSubString {
         System.out.println(i);
     }
 }
+
+/**
+ * 左右双指针（L，R）在起始点，R向右逐位滑动循环
+ * <p>
+ * ——每次滑动过程中
+ * <p>
+ * 窗内元素满足条件，R向右扩大窗口，并更新最优结果
+ * 窗内元素不满足条件，L向右缩小窗口
+ * ——R到达结尾
+ *
+ *
+ *
+ * 1. 初始化 left, right,  currentResult, bestResult
+ * 2. while (右指针没有到达结尾) {
+ *      加入right对应元素的值，更新currentResult
+ *      while (currentResult 不满足要求) {
+ *         1 窗口缩小，left右移 left++;
+ *     }
+ *     1》更新最优结果 bestResult
+ *     2》窗口扩大 right右移 right++;
+ *    }
+ * 3. 返回bestResult
+ */
